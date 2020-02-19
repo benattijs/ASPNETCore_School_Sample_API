3fd8a60903ba1777b792129dc4f5118a6340d485
Jonatah <benattijs@gmail.com>
2020-02-19 16:55:45 +1300

    First Commit of sample API.

---
+++ SchoolAPI/Controllers/SchoolsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SchoolAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SchoolsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var school = new[]{
+            new {
+                Name = "CFJL",
+                Level = "High School",
+                Location = "Horizontina"
+
+            },
+            new {
+                Name = "FAHOR",
+                Level = "University",
+                Location = "Horizontina"
+
+            },
+            new {
+                Name = "SETREM",
+                Level = "University",
+                Location = "Tres de Maio"
+
+            }
+            };
+                            
+
+            return Ok(school);
+        }
+    }
+}
---
+++ SchoolAPI/Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SchoolAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var students = new[]{
+            new {
+                Name = "John Mike",
+                Grade = 9.0,
+                School = "CFJL"
+
+            },
+            new {
+                Name = "Mark Phillips",
+                Grade = 6.0,
+                School = "CFJL"
+
+            },
+            new {
+                Name = "David Steven",
+                Grade = 8.0,
+                School = "SETREM"
+
+            },
+            new {
+                Name = "Peter Pan",
+                Grade = 8.5,
+                School = "FAHOR"
+
+            }
+            };
+                            
+
+            return Ok(students);
+        }
+    }
+}
